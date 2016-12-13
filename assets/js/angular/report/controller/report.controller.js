c98a6bc545febfa65219adbe46263c46b3de4801
lorenz_rein <lorenz.rein84@gmail.com>
2016-12-13 18:09:54 +0100

    WIP - update Order Report page

--- assets/js/angular/report/controller/report.controller.js
+++ assets/js/angular/report/controller/report.controller.js
@@ -1,28 +1,76 @@
 angular.module('app.report')
-  .controller('ReportController', function ($scope, $location, $cookies, ReportService) {
+    .controller('OrderReportController', function($scope, $location, $cookies, ReportService, EventService) {
+
+        var myStore = new store();
+
+        $scope.init = function() {
+            if (typeof($cookies.token) == 'undefined' || $cookies.token == '') {
+                $scope.logout();
+                return false;
+            }
+
+            $scope.events = myStore.events;
+            $scope.order_report = myStore.order_report;
+            $scope.selectedEvent = 'All Events';
+
+            $scope.get_order_report();
+        };
+
+        $scope.logout = function() {
+            $cookies.token = '';
+            $location.path('/log_in');
+        };
+
+        $scope.changeEvent = function(selectedEvent) {
+            $scope.selectedEvent = selectedEvent;
+        };
+
+        $scope.get_order_report = function() {
+            var reqData = {
+                // page: $scope.page,
+                // event_id: $scope.event_id
+            };
+
+            ReportService.get_order_report(reqData).then(function(response) {
+                var data = response.data;
+                if (data.status_code == 200) {
+                    $scope.order_report = data.data;
+                } else if (data.status_code == 101) {
+                    $scope.logout();
+                } else {
+                    $scope.alerts = [{ type: 'danger', msg: (angular.isString(data.message) ? data.message : 'Input Error!') }];
+                }
+            }).catch(function(error) {
+                $scope.alerts = [{ type: 'danger', msg: error }];
+            });
+        };
+
+        $scope.init();
+    })
+
+.controller('TicketReportController', function($scope, $location, $cookies, ReportService) {
 
     var myStore = new store();
-    
-    $scope.init = function() {
-      if(typeof($cookies.token) == 'undefined' || $cookies.token == '') {
-        $scope.logout();
-        return false;
-      }
 
-      $scope.events = myStore.events;
-      $scope.cart_tickets = myStore.cart_tickets;
-      $scope.order_report = myStore.order_report;
+    $scope.init = function() {
+        if (typeof($cookies.token) == 'undefined' || $cookies.token == '') {
+            $scope.logout();
+            return false;
+        }
+
+        $scope.events = myStore.events;
+        $scope.cart_tickets = myStore.cart_tickets;
     };
 
     $scope.logout = function() {
-      $cookies.token = '';
-      $location.path('/log_in');
+        $cookies.token = '';
+        $location.path('/log_in');
     }
-    
+
     $scope.changeEvent = function(selectedEvent) {
-      $scope.selectedEvent = selectedEvent;
+        $scope.selectedEvent = selectedEvent;
     }
-    
+
     $scope.selectedEvent = 'All Events';
 
     $scope.init();
--- assets/js/angular/report/services/report.service.js
+++ assets/js/angular/report/services/report.service.js
@@ -1,9 +1,20 @@
 angular.module('app.report')
-  .factory('ReportService', function($http) {
+    .factory('ReportService', function($http, $cookies) {
 
-    var Report = {
+        var Report = {
+            get_order_report: function(data) {
+                return $http.get('http://staging.ticketvow.com/api/getReportOrders?token=' + $cookies.token, data).success(function(res) {
+                    Report = res.data;
+                })
+            },
 
-    };
+            get_ticket_report: function(data) {
+                return $http.get('http://staging.ticketvow.com/api/getOrderEventList?token=' + $cookies.token, data).success(function(res) {
+                    Report = res.data;
+                })
+            },
 
-    return Report;
-  });
+        };
+
+        return Report;
+    });
