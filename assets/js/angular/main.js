7e1d6d24fdcf8027adf84f13bfb327b761e3c284
lorenz_rein <lorenz.rein84@gmail.com>
2016-12-28 18:25:53 +0100

    WIP - added Event Marketing Information page

--- assets/js/angular/main.js
+++ assets/js/angular/main.js
@@ -26,6 +26,7 @@ angular.module('angula').config(['$routeProvider', function($routeProvider, $coo
         .when("/printed_ticket", { templateUrl: "view/partials/information/printed_ticket.html", controller: "AccordionCtrl" })
         .when("/box_office", { templateUrl: "view/partials/information/box_office.html", controller: "AccordionCtrl" })
         .when("/ticket_scanning", { templateUrl: "view/partials/information/ticket_scanning.html", controller: "AccordionCtrl" })
+        .when("/event_marketing", { templateUrl: "view/partials/information/event_marketing.html", controller: "AccordionCtrl" })
 }])
 
 .run(['$rootScope', '$location', '$cookies', function($rootScope, $location, $cookies) {
@@ -47,6 +48,7 @@ angular.module('angula').config(['$routeProvider', function($routeProvider, $coo
             case '/printed_ticket':
             case '/box_office':
             case '/ticket_scanning':
+            case '/event_marketing':
                 return;
             default: // invalid URL
                 break;
